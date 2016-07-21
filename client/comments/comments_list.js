75feac764b52e57c5ccc9e466b9c31f3fe6eaa46
jose <jose>
2016-07-21 11:01:53 -0400

    Sort comments

--- client/comments/comments_list.js
+++ client/comments/comments_list.js
@@ -2,6 +2,6 @@
 Template.comments_list.helpers({
 		comments: function () {
     var website_id = this.id;
-    return Comments.find({ websiteId: website_id });
+    return Comments.find({ websiteId: website_id }, {sort:{createdOn: -1}});
 		}
 });
